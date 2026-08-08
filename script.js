7e54bdd21c87417b92c51329ef487fcac191f0cd
gramkheda <gramkheda@gmail.com>
2026-08-08 12:32:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,5 @@
-const railingPhotos = [
-    "images/railing1.jpeg",
-    "images/railing2.jpeg",
-    "images/railing3.jpeg",
-    "images/railing4.jpeg",
-    "images/railing5.jpeg",
-    "images/railing6.jpeg"
-];
-
-let railingPhotoIndex = 0;
-
-function showRailingPhoto(index) {
-    railingPhotoIndex = index;
-
-    document.getElementById("railingMainImage").src =
-        railingPhotos[railingPhotoIndex];
-
-    updateRailingThumbnails();
-}
-
-function changeRailingPhoto(direction) {
-    railingPhotoIndex += direction;
-
-    if (railingPhotoIndex >= railingPhotos.length) {
-        railingPhotoIndex = 0;
-    }
-
-    if (railingPhotoIndex < 0) {
-        railingPhotoIndex = railingPhotos.length - 1;
-    }
-
-    document.getElementById("railingMainImage").src =
-        railingPhotos[railingPhotoIndex];
-
-    updateRailingThumbnails();
-}
-
-function updateRailingThumbnails() {
-    const thumbnails =
-        document.querySelectorAll(".gallery-thumbnails img");
-
-    thumbnails.forEach((thumbnail, index) => {
-        thumbnail.classList.toggle(
-            "active",
-            index === railingPhotoIndex
-        );
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    updateRailingThumbnails();
-});
+const railingPhotos = ["images/railing1.jpeg","images/railing2.jpeg","images/railing3.jpeg","images/railing4.jpeg","images/railing5.jpeg","images/railing6.jpeg"];
+let railingPhotoIndex = 0;function showRailingPhoto(index) {railingPhotoIndex = index;document.getElementById("railingMainImage").src =railingPhotos[railingPhotoIndex];updateRailingThumbnails();}function changeRailingPhoto(direction) 
+{railingPhotoIndex += direction;if (railingPhotoIndex >= railingPhotos.length) {railingPhotoIndex = 0;}if (railingPhotoIndex < 0) {railingPhotoIndex = railingPhotos.length - 1;}document.getElementById("railingMainImage").src =railingPhotos[railingPhotoIndex];updateRailingThumbnails();}
+function updateRailingThumbnails() {const thumbnails =document.querySelectorAll(".gallery-thumbnails img");thumbnails.forEach((thumbnail, index) => {thumbnail.classList.toggle("active",index === railingPhotoIndex);});}
+document.addEventListener("DOMContentLoaded", function () {updateRailingThumbnails();});
