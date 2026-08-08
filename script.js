ab100a811dcea17b0034de56554f4452ead55101
gramkheda <gramkheda@gmail.com>
2026-08-08 12:56:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
-const railingPhotos = ["images/railing1.jpeg","images/railing2.jpeg","images/railing3.jpeg","images/railing4.jpeg","images/railing5.jpeg","images/railing6.jpeg"];
-let railingPhotoIndex = 0;function showRailingPhoto(index) {railingPhotoIndex = index;document.getElementById("railingMainImage").src =railingPhotos[railingPhotoIndex];updateRailingThumbnails();}function changeRailingPhoto(direction) 
-{railingPhotoIndex += direction;if (railingPhotoIndex >= railingPhotos.length) {railingPhotoIndex = 0;}if (railingPhotoIndex < 0) {railingPhotoIndex = railingPhotos.length - 1;}document.getElementById("railingMainImage").src =railingPhotos[railingPhotoIndex];updateRailingThumbnails();}
-function updateRailingThumbnails() {const thumbnails =document.querySelectorAll(".gallery-thumbnails img");thumbnails.forEach((thumbnail, index) => {thumbnail.classList.toggle("active",index === railingPhotoIndex);});}
-document.addEventListener("DOMContentLoaded", function () {updateRailingThumbnails();});
+const railingPhotos = [
+    "images/railing1.jpeg",
+    "images/railing2.jpeg",
+    "images/railing3.jpeg",
+    "images/railing4.jpeg",
+    "images/railing5.jpeg",
+    "images/railing6.jpeg"
+];
+
+let railingPhotoIndex = 0;
+
+function showRailingPhoto(index) {
+    railingPhotoIndex = index;
+
+    const mainImage = document.getElementById("railingMainImage");
+
+    if (mainImage) {
+        mainImage.src = railingPhotos[railingPhotoIndex];
+    }
+
+    updateRailingThumbnails();
+}
+
+function changeRailingPhoto(direction) {
+    railingPhotoIndex += direction;
+
+    if (railingPhotoIndex >= railingPhotos.length) {
+        railingPhotoIndex = 0;
+    }
+
+    if (railingPhotoIndex < 0) {
+        railingPhotoIndex = railingPhotos.length - 1;
+    }
+
+    const mainImage = document.getElementById("railingMainImage");
+
+    if (mainImage) {
+        mainImage.src = railingPhotos[railingPhotoIndex];
+    }
+
+    updateRailingThumbnails();
+}
+
+function updateRailingThumbnails() {
+    const thumbnails =
+        document.querySelectorAll(".gallery-thumbnails img");
+
+    thumbnails.forEach(function (thumbnail, index) {
+        if (index === railingPhotoIndex) {
+            thumbnail.classList.add("active");
+        } else {
+            thumbnail.classList.remove("active");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    updateRailingThumbnails();
+});
